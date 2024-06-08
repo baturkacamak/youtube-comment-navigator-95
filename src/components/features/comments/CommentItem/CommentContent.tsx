150ee50df64586d023eb26edd476589dce0ca22b
baturkacamak <batisonline@gmail.com>
2024-06-08 19:53:08 +0200

    feat: Add keyword highlighting to comment content
    - Updated src/components/features/comments/CommentItem/CommentContent.tsx:
      - Integrated highlightText utility to highlight keywords in comment content.
      - Added useSelector to fetch the current keyword from the Redux store.
      - Combined keyword highlighting with timestamp parsing for enhanced comment display.
    
    These changes enhance the comment display by highlighting search keywords, improving user experience and readability.

--- src/components/features/comments/CommentItem/CommentContent.tsx
+++ src/components/features/comments/CommentItem/CommentContent.tsx
@@ -1,12 +1,18 @@
-// src/components/features/comments/CommentItem/CommentContent.tsx
-
 import React from 'react';
-import {parseTimestamps} from '../../../../utils/parseTimestamps';
-
-import {CommentContentProps} from "../../../../types/commentTypes";
+import { parseTimestamps } from '../../../../utils/parseTimestamps';
+import { highlightText } from '../../../../utils/highlightText';
+import { CommentContentProps } from "../../../../types/commentTypes";
+import { useSelector } from 'react-redux';
+import { RootState } from "../../../../types/rootState";
 
 const CommentContent: React.FC<CommentContentProps> = ({ content, handleTimestampClick }) => {
-    return <p className="text-gray-800 dark:text-gray-200 mb-4">{parseTimestamps(content, handleTimestampClick)}</p>;
+    const keyword = useSelector((state: RootState) => state.filters.keyword);
+
+    return (
+        <p className="text-gray-800 dark:text-gray-200 mb-4">
+            {highlightText(parseTimestamps(content, handleTimestampClick), keyword)}
+        </p>
+    );
 };
 
 export default CommentContent;
