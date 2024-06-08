d307d0d4e5ff5060c0e8bd1af99a88b399d25a62
baturkacamak <batisonline@gmail.com>
2024-06-08 19:46:29 +0200

    refactor: Replace extractVideoId utility with extractYouTubeVideoIdFromUrl
    - Deleted src/utils/extractVideoId.ts which provided a basic method to extract video ID from the current URL.
    - Added src/utils/extractYouTubeVideoIdFromUrl.ts for a more flexible and robust solution to extract YouTube video IDs from URLs.
    - The new utility function supports an optional URL parameter and defaults to the current window location if not provided.
    - Utilizes the URL object to parse the URL and extract the video ID from the 'v' query parameter.
    
    These changes enhance the utility's flexibility and reliability, allowing it to handle various scenarios more effectively.

--- src/utils/extractVideoId.ts
+++
@@ -1,6 +0,0 @@
-// src/utils/extractVideoId.ts
-export const extractVideoId = (): string | null => {
-    const url = window.location.href;
-    const videoIdMatch = url.match(/[?&]v=([^&]+)/);
-    return videoIdMatch ? videoIdMatch[1] : null;
-};
