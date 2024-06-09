6045db894a1a8adc8b8f5491c35c0ac60765567f
baturkacamak <batisonline@gmail.com>
2024-06-09 17:09:52 +0200

    Refactor: Update SortList component for improved rendering
    - Wrapped individual `RadioFilter` components in `div` elements with unique keys to resolve React key warning.
    - Formatted the `SortList` component for better readability.
    - Ensured consistent usage of object destructuring in `SortListProps`.
    
    This change addresses the React warning about missing keys in lists and improves the maintainability of the component.

--- src/components/features/sidebar/SortList.tsx
+++ src/components/features/sidebar/SortList.tsx
@@ -14,52 +14,77 @@ import useAnimation from '../../../hooks/useAnimation';
 
 import {SortListProps} from "../../../types/filterTypes";
 
-const SortList: React.FC<SortListProps> = ({ filters, setFilters }) => {
-    const { triggerAnimation, getAnimationClass } = useAnimation(1000);
+const SortList: React.FC<SortListProps> = ({filters, setFilters}) => {
+    const {triggerAnimation, getAnimationClass} = useAnimation(1000);
 
     const handleRadioChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         const newSortBy = e.target.value;
         const newSortOrder = filters.sortOrder;
-        setFilters({ ...filters, sortBy: newSortBy, sortOrder: newSortOrder });
+        setFilters({...filters, sortBy: newSortBy, sortOrder: newSortOrder});
         triggerAnimation(newSortBy);
     };
 
     const toggleSortOrder = () => {
         const newSortOrder = filters.sortOrder === 'asc' ? 'desc' : 'asc';
-        setFilters({ ...filters, sortOrder: newSortOrder });
+        setFilters({...filters, sortOrder: newSortOrder});
     };
 
     const handleRandomChange = () => {
         const newSortOrder = filters.sortOrder === 'asc' ? 'desc' : 'asc';
-        setFilters({ ...filters, sortBy: 'random', sortOrder: newSortOrder });
+        setFilters({...filters, sortBy: 'random', sortOrder: newSortOrder});
         triggerAnimation('random');
     };
 
     const sortOptions = [
-        { name: 'Likes', icon: <HandThumbUpIcon className={`w-5 h-5 mr-2 ${getAnimationClass('likes', 'heartBeat')}`} />, value: 'likes' },
-        { name: 'Length', icon: <ChartBarIcon className={`w-5 h-5 mr-2 ${getAnimationClass('length', 'flash')}`} />, value: 'length' },
-        { name: 'Replies', icon: <ChatBubbleLeftRightIcon className={`w-5 h-5 mr-2 ${getAnimationClass('replies', 'bounceIn')}`} />, value: 'replies' },
-        { name: 'Date', icon: <ClockIcon className={`w-5 h-5 mr-2 ${getAnimationClass('date', 'swing')}`} />, value: 'date' },
-        { name: 'Author', icon: <UserIcon className={`w-5 h-5 mr-2 ${getAnimationClass('author', 'rubberBand')}`} />, value: 'author' },
-        { name: 'Random', icon: <ArrowsUpDownIcon className={`w-5 h-5 mr-2 ${getAnimationClass('random', 'flip')}`} />, value: 'random' },
+        {
+            name: 'Likes',
+            icon: <HandThumbUpIcon className={`w-5 h-5 mr-2 ${getAnimationClass('likes', 'heartBeat')}`}/>,
+            value: 'likes'
+        },
+        {
+            name: 'Length',
+            icon: <ChartBarIcon className={`w-5 h-5 mr-2 ${getAnimationClass('length', 'flash')}`}/>,
+            value: 'length'
+        },
+        {
+            name: 'Replies',
+            icon: <ChatBubbleLeftRightIcon className={`w-5 h-5 mr-2 ${getAnimationClass('replies', 'bounceIn')}`}/>,
+            value: 'replies'
+        },
+        {
+            name: 'Date',
+            icon: <ClockIcon className={`w-5 h-5 mr-2 ${getAnimationClass('date', 'swing')}`}/>,
+            value: 'date'
+        },
+        {
+            name: 'Author',
+            icon: <UserIcon className={`w-5 h-5 mr-2 ${getAnimationClass('author', 'rubberBand')}`}/>,
+            value: 'author'
+        },
+        {
+            name: 'Random',
+            icon: <ArrowsUpDownIcon className={`w-5 h-5 mr-2 ${getAnimationClass('random', 'flip')}`}/>,
+            value: 'random'
+        },
     ];
 
     return (
-        <div className="mb-4">
+        <>
             {sortOptions.map(option => (
-                <RadioFilter
-                    key={option.value}
-                    name={option.name}
-                    icon={option.icon}
-                    value={option.value}
-                    selectedValue={filters.sortBy}
-                    sortOrder={filters.sortOrder}
-                    isRandom={option.value === 'random'}
-                    onChange={option.value === 'random' ? handleRandomChange : handleRadioChange}
-                    onToggleSortOrder={option.value === 'random' ? handleRandomChange : toggleSortOrder}
-                />
-            ))}
-        </div>
+                    <div key={option.value}>
+                        <RadioFilter
+                            name={option.name}
+                            icon={option.icon}
+                            value={option.value}
+                            selectedValue={filters.sortBy}
+                            sortOrder={filters.sortOrder}
+                            isRandom={option.value === 'random'}
+                            onChange={option.value === 'random' ? handleRandomChange : handleRadioChange}
+                            onToggleSortOrder={option.value === 'random' ? handleRandomChange : toggleSortOrder}
+                        />
+                    </div>
+                ))}
+        </>
     );
 };
 
