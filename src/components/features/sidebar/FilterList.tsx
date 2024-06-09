d5c9e684ce5ea74269796e0a3ed3af4113cb97e3
baturkacamak <batisonline@gmail.com>
2024-06-09 17:10:34 +0200

    Refactor: Improve FilterList layout and readability
    - Updated FilterList to use a more flexible layout structure.
    - Adjusted filter options to be rendered within individual divs.
    - Removed unnecessary bottom margin to better align with new layout.
    - Simplified code structure for better readability and maintainability.
    
    These changes enhance the overall UI/UX by making the filter options more visually organized and easier to interact with.

--- src/components/features/sidebar/FilterList.tsx
+++ src/components/features/sidebar/FilterList.tsx
@@ -7,36 +7,40 @@ import useAnimation from '../../../hooks/useAnimation';
 
 import {FilterListProps} from "../../../types/filterTypes";
 
-const FilterList: React.FC<FilterListProps> = ({ filters, setFilters }) => {
-    const { triggerAnimation, getAnimationClass } = useAnimation(1000);
+const FilterList: React.FC<FilterListProps> = ({filters, setFilters}) => {
+    const {triggerAnimation, getAnimationClass} = useAnimation(1000);
 
     const handleCheckboxChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        const { name, checked } = e.target;
-        setFilters({ ...filters, [name]: checked });
+        const {name, checked} = e.target;
+        setFilters({...filters, [name]: checked});
         triggerAnimation(name);
     };
 
     const filterOptions = [
-        { name: 'timestamps', icon: <ClockIcon className={`w-5 h-5 mr-2 ${getAnimationClass('timestamps', 'swing')}`} /> },
-        { name: 'heart', icon: <HeartIcon className={`w-5 h-5 mr-2 ${getAnimationClass('heart', 'heartBeat')}`} /> },
-        { name: 'links', icon: <LinkIcon className={`w-5 h-5 mr-2 ${getAnimationClass('links', 'flash')}`} /> },
-        { name: 'members', icon: <UserGroupIcon className={`w-5 h-5 mr-2 ${getAnimationClass('members', 'rubberBand')}`} /> },
-        { name: 'donated', icon: <GiftIcon className={`w-5 h-5 mr-2 ${getAnimationClass('donated', 'bounceIn')}`} /> },
+        {name: 'timestamps', icon: <ClockIcon className={`w-5 h-5 mr-2 ${getAnimationClass('timestamps', 'swing')}`}/>},
+        {name: 'heart', icon: <HeartIcon className={`w-5 h-5 mr-2 ${getAnimationClass('heart', 'heartBeat')}`}/>},
+        {name: 'links', icon: <LinkIcon className={`w-5 h-5 mr-2 ${getAnimationClass('links', 'flash')}`}/>},
+        {
+            name: 'members',
+            icon: <UserGroupIcon className={`w-5 h-5 mr-2 ${getAnimationClass('members', 'rubberBand')}`}/>
+        },
+        {name: 'donated', icon: <GiftIcon className={`w-5 h-5 mr-2 ${getAnimationClass('donated', 'bounceIn')}`}/>},
     ];
 
     return (
-        <div className="mb-2">
+        <>
             {filterOptions.map(option => (
-                <CheckboxFilter
-                    key={option.name}
-                    name={option.name}
-                    icon={option.icon}
-                    value={option.name}
-                    checked={filters[option.name]}
-                    onChange={handleCheckboxChange}
-                />
+                <div key={option.name}>
+                    <CheckboxFilter
+                        name={option.name}
+                        icon={option.icon}
+                        value={option.name}
+                        checked={filters[option.name]}
+                        onChange={handleCheckboxChange}
+                    />
+                </div>
             ))}
-        </div>
+        </>
     );
 };
 
