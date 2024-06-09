98780f8db92efbb7b57c4711501d7cd78d0bef98
baturkacamak <batisonline@gmail.com>
2024-06-09 19:59:51 +0200

    refactor(LoadingSection): Make the loading section more compact and user-friendly
    - Refactored the layout of the `LoadingSection` component to be more compact.
    - Moved button labels to include the counts within parentheses.
    - Adjusted the button styles for consistency and readability.
    - Used a flexbox layout to better utilize space and improve design.

--- src/components/features/loading/LoadingSection.tsx
+++ src/components/features/loading/LoadingSection.tsx
@@ -5,31 +5,41 @@ import {
     DocumentTextIcon,
     InboxIcon
 } from '@heroicons/react/24/outline';
-import Button from '../../common/Button';
 import Box from "../../common/Box";
-
-import {LoadingSectionProps} from "../../../types/layoutTypes";
+import Button from "../../common/Button";
+import { LoadingSectionProps } from "../../../types/layoutTypes";
 
 const LoadingSection: React.FC<LoadingSectionProps> = ({
                                                            onLoadComments, onLoadChat, onLoadTranscript, onLoadAll,
                                                            commentsCount, repliesCount, transcriptsCount
                                                        }) => {
     return (
-        <Box>
-            <div className="flex items-center justify-around mb-4">
-                <Button onClick={() => onLoadComments(true)} icon={ChatBubbleOvalLeftIcon} label="Load Comments"
-                        className="bg-teal-700 hover:bg-teal-600 active:bg-teal-800 text-white"/>
-                <Button onClick={onLoadChat} icon={InboxIcon} label="Load Chat Replies"
-                        className="bg-teal-700 hover:bg-teal-600 active:bg-teal-800 text-white"/>
-                <Button onClick={onLoadTranscript} icon={DocumentTextIcon} label="Load Transcript"
-                        className="bg-teal-700 hover:bg-teal-600 active:bg-teal-800 text-white"/>
-                <Button onClick={() => onLoadAll(true)} icon={ClipboardDocumentListIcon} label="Load All"
-                        className="bg-teal-700 hover:bg-teal-600 active:bg-teal-800 text-white"/>
-            </div>
-            <div className="flex items-center justify-around text-gray-800 dark:text-gray-200 text-sm">
-                <span>Comments Loaded: {commentsCount}</span>
-                <span>Chat Replies Loaded: {repliesCount}</span>
-                <span>Transcripts Loaded: {transcriptsCount}</span>
+        <Box className="p-1 bg-gradient-to-r from-teal-100 to-teal-300 dark:bg-gradient-to-r dark:from-teal-700 dark:to-teal-900 rounded-lg shadow-md">
+            <div className="flex items-center justify-between space-x-4">
+                <Button
+                    onClick={() => onLoadComments(true)}
+                    icon={ChatBubbleOvalLeftIcon}
+                    label={`Load Comments (${commentsCount})`}
+                    className="bg-teal-700 hover:bg-teal-600 active:bg-teal-800 text-white rounded-full py-1 px-2 text-xs"
+                />
+                <Button
+                    onClick={onLoadChat}
+                    icon={InboxIcon}
+                    label={`Load Chat (${repliesCount})`}
+                    className="bg-teal-700 hover:bg-teal-600 active:bg-teal-800 text-white rounded-full py-1 px-2 text-xs"
+                />
+                <Button
+                    onClick={onLoadTranscript}
+                    icon={DocumentTextIcon}
+                    label={`Load Transcript (${transcriptsCount})`}
+                    className="bg-teal-700 hover:bg-teal-600 active:bg-teal-800 text-white rounded-full py-1 px-2 text-xs"
+                />
+                <Button
+                    onClick={() => onLoadAll(true)}
+                    icon={ClipboardDocumentListIcon}
+                    label="Load All"
+                    className="bg-teal-700 hover:bg-teal-600 active:bg-teal-800 text-white rounded-full py-1 px-2 text-xs"
+                />
             </div>
         </Box>
     );
