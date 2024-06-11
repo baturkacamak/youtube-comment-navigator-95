f049ddc4661d7a2239a289bbf81d39b2aa18b7f2
baturkacamak <batisonline@gmail.com>
2024-06-11 15:38:06 +0200

    feat: Add CACHE_KEYS constants for caching video comments
    - Introduced CACHE_KEYS object to store keys for caching final, temporary, and continuation token comments.
    - Updated `environmentVariables.ts` to include the new CACHE_KEYS constants for efficient comment caching.

--- src/utils/environmentVariables.ts
+++ src/utils/environmentVariables.ts
@@ -2,3 +2,9 @@
 export const isLocalEnvironment = (): boolean => {
     return process.env.NODE_ENV === 'development';
 };
+
+export const CACHE_KEYS = {
+    FINAL: (videoId: string) => `cachedComments_${videoId}`,
+    TEMP: (videoId: string) => `tempCachedComments_${videoId}`,
+    CONTINUATION_TOKEN: (videoId: string) => `continuationToken_${videoId}`
+};
