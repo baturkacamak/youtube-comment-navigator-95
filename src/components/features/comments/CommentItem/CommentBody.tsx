1aab4d3225335ddd1509bc8b6b9ddfb67f0c65a8
baturkacamak <batisonline@gmail.com>
2024-06-12 13:53:24 +0200

    refactor: Enhance CommentBody with linkify, timestamp parsing, and text highlighting
    - Imported linkifyText function for hyperlinking URLs within comments.
    - Refactored CommentBody to apply linkifyText, parseTimestamps, and highlightText sequentially to comment content.
    - Simplified rendering logic by assigning the processed content to a single variable before rendering.

--- src/components/features/comments/CommentItem/CommentBody.tsx
+++ src/components/features/comments/CommentItem/CommentBody.tsx
@@ -1,18 +1,21 @@
 import React from 'react';
 import { parseTimestamps } from '../../../../utils/parseTimestamps';
 import { highlightText } from '../../../../utils/highlightText';
+import { linkifyText } from '../../../../utils/linkifyText';
 import { CommentContentProps } from "../../../../types/commentTypes";
 import { useSelector } from 'react-redux';
 import { RootState } from "../../../../types/rootState";
-import {linkifyText} from "../../../../utils/linkifyText";
-
 
 const CommentBody: React.FC<CommentContentProps> = ({ content, handleTimestampClick }) => {
     const keyword = useSelector((state: RootState) => state.filters.keyword);
 
+    const linkedText = linkifyText(content);
+    const timestampedText = parseTimestamps(linkedText, handleTimestampClick);
+    const highlightedText = highlightText(timestampedText, keyword);
+
     return (
         <p className="text-xl mb-2">
-            {highlightText(parseTimestamps(linkifyText(content), handleTimestampClick), keyword)}
+            {highlightedText}
         </p>
     );
 };
