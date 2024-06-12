b5e9e2dd595c98b13f3d284a0fdb95bd3eb17250
baturkacamak <batisonline@gmail.com>
2024-06-12 13:54:26 +0200

    fix: Enhance SelectBox component
    - Improved keyboard accessibility in SelectBox to prevent browser scrollbar movement during selection.
    - Added `buttonClassName` prop to SelectBox for customizable button styles.
    - Updated SelectBox to include smooth transition animations using Tailwind classes for dropdown menu.

--- src/components/common/SelectBox.tsx
+++ src/components/common/SelectBox.tsx
@@ -1,11 +1,10 @@
 // src/components/SelectBox.tsx
-import React, {useEffect, useRef, useState} from 'react';
-import {ChevronDownIcon} from '@heroicons/react/24/outline';
+import React, { useEffect, useRef, useState } from 'react';
+import { ChevronDownIcon } from '@heroicons/react/24/outline';
+import { SelectBoxProps } from "../../types/filterTypes";
+import { Option } from "../../types/utilityTypes";
 
-import {SelectBoxProps} from "../../types/filterTypes";
-import {Option} from "../../types/utilityTypes";
-
-const SelectBox: React.FC<SelectBoxProps> = ({ options, selectedOption, setSelectedOption }) => {
+const SelectBox: React.FC<SelectBoxProps> = ({ options, selectedOption, setSelectedOption, buttonClassName }) => {
     const [isOpen, setIsOpen] = useState(false);
     const [highlightedIndex, setHighlightedIndex] = useState(0);
     const menuRef = useRef<HTMLDivElement>(null);
@@ -19,16 +18,20 @@ const SelectBox: React.FC<SelectBoxProps> = ({ options, selectedOption, setSelec
     const handleKeyDown = (event: React.KeyboardEvent) => {
         switch (event.key) {
             case 'ArrowDown':
+                event.preventDefault(); // Prevent default behavior
                 setHighlightedIndex((prevIndex) => (prevIndex + 1) % options.length);
                 break;
             case 'ArrowUp':
+                event.preventDefault(); // Prevent default behavior
                 setHighlightedIndex((prevIndex) => (prevIndex - 1 + options.length) % options.length);
                 break;
             case 'Enter':
+                event.preventDefault(); // Prevent default behavior
                 setSelectedOption(options[highlightedIndex]);
                 setIsOpen(false);
                 break;
             case 'Escape':
+                event.preventDefault(); // Prevent default behavior
                 setIsOpen(false);
                 break;
         }
@@ -59,11 +62,11 @@ const SelectBox: React.FC<SelectBoxProps> = ({ options, selectedOption, setSelec
         <div className="relative inline-block text-left w-48 h-10">
             <button
                 type="button"
-                className="inline-flex h-10 justify-between w-full rounded-l-lg shadow-sm px-4 py-2 bg-teal-200 dark:bg-gray-700 text-gray-800 dark:text-gray-200 text-sm font-medium hover:bg-gray-100 dark:hover:bg-gray-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-600"
+                className={`inline-flex h-10 justify-between w-full shadow-sm px-4 py-2 bg-teal-200 dark:bg-gray-700 text-gray-800 dark:text-gray-200 text-sm font-medium hover:bg-gray-100 dark:hover:bg-gray-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-600 ${buttonClassName || 'rounded-lg'}`}
                 id="options-menu"
                 aria-haspopup="true"
                 aria-expanded={isOpen}
-                onClick={() => setIsOpen(!isOpen)}
+                onClick={() => setIsOpen(prevIsOpen => !prevIsOpen)}
             >
                 <span className="flex items-center">
                     <selectedOption.icon className="w-5 h-5 mr-2" />
@@ -72,8 +75,8 @@ const SelectBox: React.FC<SelectBoxProps> = ({ options, selectedOption, setSelec
                 <ChevronDownIcon className="w-5 h-5" />
             </button>
             <div
-                className={`origin-top-left absolute z-20 left-0 mt-2 w-full rounded-md shadow-lg bg-teal-200 dark:bg-gray-700 ring-1 ring-black ring-opacity-5 transition-all transform ${
-                    isOpen ? 'scale-100 opacity-100' : 'scale-95 opacity-0'
+                className={`origin-top-left absolute z-20 left-0 mt-2 w-full rounded-md shadow-lg bg-teal-200 dark:bg-gray-700 ring-1 ring-black ring-opacity-5 transform transition-all duration-300 ease-in-out ${
+                    isOpen ? 'opacity-100 translate-y-0' : 'opacity-0 -translate-y-2 pointer-events-none'
                 }`}
                 ref={menuRef}
                 tabIndex={-1}
@@ -82,23 +85,21 @@ const SelectBox: React.FC<SelectBoxProps> = ({ options, selectedOption, setSelec
                 aria-orientation="vertical"
                 aria-labelledby="options-menu"
             >
-                {isOpen && (
-                    <div className="py-1 transition-all duration-300 ease-in-out">
-                        {options.map((option, index) => (
-                            <button
-                                key={option.value}
-                                onClick={() => handleOptionClick(option, index)}
-                                className={`flex items-center px-4 py-2 text-sm text-gray-800 dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-600 hover:text-gray-900 w-full ${
-                                    highlightedIndex === index ? 'bg-gray-100 dark:bg-gray-600' : ''
-                                } transition-all duration-300 ease-in-out`}
-                                role="menuitem"
-                            >
-                                <option.icon className="w-5 h-5 mr-2" />
-                                {option.label}
-                            </button>
-                        ))}
-                    </div>
-                )}
+                <div className="py-1">
+                    {options.map((option, index) => (
+                        <button
+                            key={option.value}
+                            onClick={() => handleOptionClick(option, index)}
+                            className={`flex items-center px-4 py-2 text-sm text-gray-800 dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-600 hover:text-gray-900 w-full ${
+                                highlightedIndex === index ? 'bg-gray-100 dark:bg-gray-600' : ''
+                            } transition-all duration-300 ease-in-out`}
+                            role="menuitem"
+                        >
+                            <option.icon className="w-5 h-5 mr-2" />
+                            {option.label}
+                        </button>
+                    ))}
+                </div>
             </div>
         </div>
     );
