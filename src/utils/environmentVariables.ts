8a09a4f61a706d160bab07ae36914236693a59c2
baturkacamak <batisonline@gmail.com>
2024-06-13 14:52:05 +0200

    feat: Add supportedLanguages array to environmentVariables
    - Added `supportedLanguages` array to centralize the list of supported languages.
    - This array includes all language codes currently supported by the application.
    - Ensured consistent file formatting by adding a newline at the end of the file.

--- src/utils/environmentVariables.ts
+++ src/utils/environmentVariables.ts
@@ -8,3 +8,9 @@ export const CACHE_KEYS = {
     TEMP: (videoId: string) => `tempCachedComments_${videoId}`,
     CONTINUATION_TOKEN: (videoId: string) => `continuationToken_${videoId}`
 };
+
+export const supportedLanguages = [
+    'ar', 'bn', 'cs', 'da', 'de', 'el', 'en', 'es', 'fa', 'fi', 'fr', 'he', 'hi', 'hu',
+    'id', 'it', 'ja', 'jv', 'ko', 'mr', 'ms', 'nl', 'no', 'pa', 'pl', 'pt', 'ro', 'ru',
+    'sk', 'sr', 'sv', 'ta', 'te', 'th', 'tl', 'tr', 'uk', 'ur', 'vi', 'zh'
+];
