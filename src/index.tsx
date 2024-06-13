fc5c83c08f5ab6d5906abeff53db320daf76c0b5
baturkacamak <batisonline@gmail.com>
2024-06-13 04:11:25 +0200

    chore: Initialize i18next in the main application entry
    - Imported and initialized i18next in `src/index.tsx` to enable internationalization.

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,7 @@ import App from './App';
 import {Provider} from 'react-redux';
 import store from './store/store';
 import {isLocalEnvironment} from "./utils/environmentVariables";
+import './i18n';
 
 const rootElement = document.getElementById('youtube-comment-navigator-app') as HTMLElement;
 
