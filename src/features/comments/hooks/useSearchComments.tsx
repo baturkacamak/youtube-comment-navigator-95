37e1d22be4b5ea18bd1bc2c1e41ea0c595b12226
baturkacamak <batisonline@gmail.com>
2024-06-16 17:06:52 +0200

    Enhance Bookmark Search Functionality and State Management
    - Updated `useAppState` hook to fetch bookmarked comments on initial load and when `showBookmarked` is toggled.
    - Modified `handleSearch` in `useSearchComments` to account for `showBookmarked` state, enabling search functionality to work with both regular and bookmarked comments.
    - Improved Redux state management for bookmarks, ensuring consistent updates and accurate search results.
    - Refactored `useSearchComments` to use `RootState` for better type safety and consistency.
    - Enhanced user experience with accurate and responsive search results, whether searching through all comments or just bookmarked ones.

--- src/features/comments/hooks/useSearchComments.tsx
+++ src/features/comments/hooks/useSearchComments.tsx
@@ -1,22 +1,26 @@
 import { useDispatch, useSelector } from 'react-redux';
-import {RootState, setComments, setCommentsCount, setFilters} from '../../../store/store';
+import { RootState, setComments, setCommentsCount, setFilters } from '../../../store/store';
 import useSortedComments from "./useSortedComments";
 import { Comment } from "../../../types/commentTypes";
 
 const useSearchComments = () => {
   const dispatch = useDispatch();
-  const originalComments = useSelector((state: any) => state.originalComments);
+  const originalComments = useSelector((state: RootState) => state.originalComments);
   const filters = useSelector((state: RootState) => state.filters);
+  const showBookmarked = useSelector((state: RootState) => state.showBookmarked);
+  const bookmarkedComments = useSelector((state: RootState) => state.bookmarkedComments);
   const { sortComments } = useSortedComments(true);
 
   const handleSearch = (keyword: string) => {
     dispatch(setFilters({ ...filters, keyword }));
 
+    const commentsToSearch = showBookmarked ? bookmarkedComments : originalComments;
+
     if (keyword.trim() === '') {
-      dispatch(setComments(originalComments));
-      dispatch(setCommentsCount(originalComments.length));
+      dispatch(setComments(commentsToSearch));
+      dispatch(setCommentsCount(commentsToSearch.length));
     } else {
-      const filteredComments = originalComments.filter((comment: Comment) =>
+      const filteredComments = commentsToSearch.filter((comment: Comment) =>
           comment.content.toLowerCase().includes(keyword.toLowerCase())
       );
       const sortedAndFilteredComments = sortComments(filteredComments, filters.sortBy, filters.sortOrder);
