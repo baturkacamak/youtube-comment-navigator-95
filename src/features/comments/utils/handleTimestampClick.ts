1e072ddddec93e6bedc07ecd3413ca41b3ce14d2
baturkacamak <batisonline@gmail.com>
2024-06-19 20:31:56 +0200

    fix: Correct handling of timestamp clicks for YouTube player
    - Updated `handleTimestampClick` utility to correctly select the YouTube player element and invoke `seekTo` method.
    - Added type assertion and method check to ensure the `seekTo` method exists on the player element.
    - Improved error handling to log appropriate messages when the player or `seekTo` method is not available.
    
    This update ensures that timestamp clicks correctly navigate the YouTube player to the specified time, improving user experience.

--- src/features/comments/utils/handleTimestampClick.ts
+++ src/features/comments/utils/handleTimestampClick.ts
@@ -1,15 +1,19 @@
-// src/utils/handleTimestampClick.ts
-
 import { MouseEvent } from 'react';
 
 const handleTimestampClick = (event: MouseEvent<HTMLAnchorElement>) => {
     event.preventDefault();
     const timestamp = event.currentTarget.getAttribute('data-timestamp');
-    if (timestamp && (window as any).yt && (window as any).yt.Player) {
-        const player = new (window as any).yt.Player('video-player'); // Replace 'video-player' with your video player ID
-        const timeParts = timestamp.split(':').map(Number);
-        const seconds = timeParts.reduce((acc, part) => acc * 60 + part, 0);
-        player.seekTo(seconds, true);
+    if (timestamp) {
+        const player = document.querySelector('#movie_player') as any;
+        if (player && typeof player.seekTo === 'function') {
+            const timeParts = timestamp.split(':').map(Number);
+            const seconds = timeParts.reduce((acc, part) => acc * 60 + part, 0);
+            player.seekTo(seconds, true);
+        } else {
+            console.error("Player does not have a seekTo method");
+        }
+    } else {
+        console.error("YouTube Player is not available");
     }
 };
 
