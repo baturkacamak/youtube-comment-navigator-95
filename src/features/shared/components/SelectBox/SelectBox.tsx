713bb01ad2c3df8a0f1aaae0c670d11a85ff412b
baturkacamak <batisonline@gmail.com>
2024-06-20 08:36:52 +0200

    Add translations for CTA message in multiple languages
    - Added the translations for "If you enjoy using this extension, please" and "rate it on the Chrome Web Store" in the following languages:
      - Arabic (ar)
      - Bengali (bn)
      - Czech (cs)
      - Danish (da)
      - German (de)
      - Greek (el)
      - Spanish (es)
      - Persian (fa)
      - Finnish (fi)
      - French (fr)
      - Hebrew (he)
      - Hindi (hi)
      - Hungarian (hu)
      - Indonesian (id)
      - Italian (it)
      - Japanese (ja)
      - Javanese (jv)
      - Korean (ko)
      - Marathi (mr)
      - Malay (ms)
      - Dutch (nl)
      - Norwegian (no)
      - Punjabi (pa)
      - Polish (pl)
      - Portuguese (pt)
      - Romanian (ro)
      - Russian (ru)
      - Slovak (sk)
      - Serbian (sr)
      - Swedish (sv)
      - Tamil (ta)
      - Telugu (te)
      - Thai (th)
      - Tagalog (tl)
      - Turkish (tr)
      - Ukrainian (uk)
      - Urdu (ur)
      - Vietnamese (vi)
      - Chinese (zh)
    
    Updated the translation.json files to include these new messages without overwriting existing content.

--- src/features/shared/components/SelectBox/SelectBox.tsx
+++ src/features/shared/components/SelectBox/SelectBox.tsx
@@ -110,7 +110,7 @@ const SelectBox: React.FC<SelectBoxProps> = ({
         <div className="relative inline-block text-left w-48 h-10">
             <button
                 type="button"
-                className={`inline-flex h-10 justify-between w-full shadow-sm px-4 py-2 bg-teal-200 dark:bg-gray-700 text-gray-800 dark:text-gray-200 text-sm font-medium hover:bg-gray-100 dark:hover:bg-gray-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-600 ${buttonClassName || 'rounded-lg'}`}
+                className={`inline-flex h-full justify-between items-center  w-full shadow-sm px-4 py-2 bg-teal-200 dark:bg-gray-700 text-gray-800 dark:text-gray-200 text-sm font-medium hover:bg-gray-100 dark:hover:bg-gray-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-600 ${buttonClassName || 'rounded-lg'}`}
                 id="options-menu"
                 aria-haspopup="true"
                 aria-expanded={isOpen}
