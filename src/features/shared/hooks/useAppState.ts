c2d2efa48732b60b1ca7ae4e04bf95107bfabc89
baturkacamak <batisonline@gmail.com>
2024-06-21 19:53:55 +0200

    Refactor useAppState: Add tab-specific state management
    - Added activeTab state to manage the currently active tab.
    - Updated useAppState to fetch bookmarked comments only when the bookmarks tab is active.
    - Removed unused imports and variables for cleaner code.
    - Ensured filteredAndSortedComments respects the active tab state.

--- src/features/shared/hooks/useAppState.ts
+++ src/features/shared/hooks/useAppState.ts
@@ -1,54 +1,47 @@
 import { useDispatch, useSelector } from 'react-redux';
-import { useCallback, useEffect, useMemo } from 'react';
+import { useCallback, useEffect, useMemo, useState } from 'react';
 import { RootState } from '../../../types/rootState';
 import { setFilters, setShowBookmarked, setBookmarkedComments } from '../../../store/store';
 import useComments from '../../comments/hooks/useComments';
 import useSortedComments from '../../comments/hooks/useSortedComments';
 import useFilteredComments from '../../comments/hooks/useFilteredComments';
 import useSearchComments from '../../comments/hooks/useSearchComments';
-import useLoadComments from '../../comments/hooks/useLoadComments';
 import { Filters } from '../../../types/filterTypes';
-import { retrieveDataFromDB } from '../../shared/utils/cacheUtils';
-import { Comment } from '../../../types/commentTypes';
+import { retrieveDataFromDB } from '../utils/cacheUtils';
 
 const useAppState = () => {
     const dispatch = useDispatch();
+    const [activeTab, setActiveTab] = useState('comments'); // New state for active tab
 
     // Using individual useSelector calls to avoid unnecessary re-renders
     const comments = useSelector((state: RootState) => state.comments);
-    const originalComments = useSelector((state: RootState) => state.originalComments);
     const filters = useSelector((state: RootState) => state.filters);
     const isLoading = useSelector((state: RootState) => state.isLoading);
     const showBookmarked = useSelector((state: RootState) => state.showBookmarked);
     const bookmarkedComments = useSelector((state: RootState) => state.bookmarkedComments);
-    useSelector((state: RootState) => state.commentsCount);
     const repliesCount = useSelector((state: RootState) => state.repliesCount);
     const transcriptsCount = useSelector((state: RootState) => state.transcriptsCount);
 
     const { sortComments } = useSortedComments(false);
     const { filterComments } = useFilteredComments(false);
     const { handleSearch } = useSearchComments();
-    const { loadComments, loadChatReplies, loadTranscript, loadAll } = useLoadComments();
-
     const { initialLoadCompleted } = useComments();
 
     useEffect(() => {
-        const fetchBookmarkedComments = async () => {
-            const bookmarks = await retrieveDataFromDB('bookmarks');
-            dispatch(setBookmarkedComments(bookmarks.data || []));
-        };
+        if (activeTab === 'bookmarks') {
+            const fetchBookmarkedComments = async () => {
+                const bookmarks = await retrieveDataFromDB('bookmarks');
+                dispatch(setBookmarkedComments(bookmarks.data || []));
+            };
 
-        fetchBookmarkedComments(); // Fetch bookmarks on initial load
-
-        if (showBookmarked) {
-            fetchBookmarkedComments(); // Fetch bookmarks when showBookmarked is toggled
+            fetchBookmarkedComments();
         }
-    }, [showBookmarked, dispatch]);
+    }, [activeTab, dispatch]);
 
     const filteredAndSortedComments = useMemo(() => {
-        const commentsToUse = showBookmarked ? bookmarkedComments : comments;
+        const commentsToUse = activeTab === 'bookmarks' ? bookmarkedComments : comments;
         return filterComments(sortComments(commentsToUse, filters.sortBy, filters.sortOrder), filters);
-    }, [filters, sortComments, filterComments, comments, showBookmarked, bookmarkedComments]);
+    }, [filters, sortComments, filterComments, comments, activeTab, bookmarkedComments]);
 
     const setFiltersCallback = useCallback((filters: Filters) => {
         dispatch(setFilters(filters));
@@ -66,14 +59,13 @@ const useAppState = () => {
         transcriptsCount,
         initialLoadCompleted,
         handleSearch,
-        loadComments,
-        loadChatReplies,
-        loadTranscript,
-        loadAll,
-        filteredAndSortedComments, // Ensure this returns the right set of comments
+        filteredAndSortedComments,
         setFiltersCallback,
         showBookmarked,
         toggleShowBookmarked,
+        activeTab, // Return the active tab
+        setActiveTab,
+        commentCount: comments.length
     };
 };
 
