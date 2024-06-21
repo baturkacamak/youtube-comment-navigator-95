5bc5cf005cd1adc252234c779aa4809cd3fa6e51
baturkacamak <batisonline@gmail.com>
2024-06-21 19:53:24 +0200

    Refactor debounce utility: Improve type safety with TypeScript generics
    - Updated debounce function to use TypeScript generics for improved type safety.
    - Ensured the debounce function correctly types the func argument and its parameters.

--- src/features/shared/utils/debounce.ts
+++ src/features/shared/utils/debounce.ts
@@ -1,8 +1,8 @@
 // src/utils/debounce.ts
-export const debounce = (func: Function, wait: number) => {
+export const debounce = <T extends (...args: any[]) => void>(func: T, wait: number) => {
     let timeout: ReturnType<typeof setTimeout>;
 
-    return (...args: any[]) => {
+    return (...args: Parameters<T>): void => {
         clearTimeout(timeout);
         timeout = setTimeout(() => func(...args), wait);
     };
