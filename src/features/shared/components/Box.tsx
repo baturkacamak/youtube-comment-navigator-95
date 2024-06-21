dc16668703cd84c5fdf896d912777fd71e34f991
baturkacamak <batisonline@gmail.com>
2024-06-21 21:18:34 +0200

    Adjust Box component padding for improved layout
    - Changed padding from `p-4` to `px-4` to only apply horizontal padding.
    - Ensured consistent styling and spacing within the Box component.

--- src/features/shared/components/Box.tsx
+++ src/features/shared/components/Box.tsx
@@ -31,7 +31,7 @@ const Box: React.FC<BoxProps> = ({
     return (
         <div
             className={classNames(
-                "p-4 transition-colors duration-500 rounded-lg shadow-md",
+                "px-4 transition-colors duration-500 rounded-lg shadow-md",
                 {
                     [bgColor]: !/^#|rgb/.test(bgColor),
                     [darkBgColor]: !/^#|rgb/.test(darkBgColor),
