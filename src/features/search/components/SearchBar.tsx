97e4c41eb1c62a10715ee4455ff3a7c61a98f10d
baturkacamak <batisonline@gmail.com>
2024-06-21 19:55:45 +0200

    Refactor SearchBar: Simplify imports and remove unnecessary Box component
    - Simplified import statements for better readability.
    - Removed the unnecessary Box component wrapping the search form.
    - Improved code formatting for consistency.

--- src/features/search/components/SearchBar.tsx
+++ src/features/search/components/SearchBar.tsx
@@ -1,6 +1,5 @@
-import React, { useEffect, useState, useCallback, FormEvent, useRef } from 'react';
+import React, {FormEvent, useCallback, useEffect, useRef, useState} from 'react';
 import SelectBox from '../../shared/components/SelectBox/SelectBox';
-import Box from '../../shared/components/Box';
 import {
     ChatBubbleOvalLeftIcon,
     ClipboardDocumentListIcon,
@@ -9,18 +8,18 @@ import {
     MagnifyingGlassIcon,
     XCircleIcon
 } from '@heroicons/react/24/outline';
-import { Option } from "../../../types/utilityTypes";
-import { useTranslation } from 'react-i18next';
+import {Option} from "../../../types/utilityTypes";
+import {useTranslation} from 'react-i18next';
 
 
-const SearchBar: React.FC<{ onSearch: (query: string) => void }> = ({ onSearch }) => {
-    const { t } = useTranslation();
+const SearchBar: React.FC<{ onSearch: (query: string) => void }> = ({onSearch}) => {
+    const {t} = useTranslation();
 
     const options: Option[] = [
-        { value: 'all', label: t('All'), icon: ClipboardDocumentListIcon },
-        { value: 'comments', label: t('Comments'), icon: ChatBubbleOvalLeftIcon },
-        { value: 'chat', label: t('Live Chat'), icon: InboxIcon },
-        { value: 'transcript', label: t('Transcript'), icon: DocumentTextIcon },
+        {value: 'all', label: t('All'), icon: ClipboardDocumentListIcon},
+        {value: 'comments', label: t('Comments'), icon: ChatBubbleOvalLeftIcon},
+        {value: 'chat', label: t('Live Chat'), icon: InboxIcon},
+        {value: 'transcript', label: t('Transcript'), icon: DocumentTextIcon},
     ];
 
     const [query, setQuery] = useState('');
@@ -67,43 +66,41 @@ const SearchBar: React.FC<{ onSearch: (query: string) => void }> = ({ onSearch }
     };
 
     return (
-        <Box>
-            <form onSubmit={handleSubmit} className="flex items-center p-2 relative" aria-label={t('Search form')}>
-                <SelectBox
-                    options={options}
-                    selectedOption={options.find(option => option.value === selectedOption.value)!}
-                    setSelectedOption={setSelectedOption}
-                    buttonClassName={'rounded-l-lg'}
-                    aria-label={t('Select search category')}
-                />
-                <input
-                    type="text"
-                    placeholder={placeholder}
-                    value={query}
-                    onChange={e => setQuery(e.target.value)}
-                    className="flex-grow p-2 bg-teal-200 dark:bg-gray-700 text-gray-800 dark:text-gray-200 focus:outline-none focus:ring-2 focus:ring-blue-600 transition-all duration-300 ease-in-out"
-                    aria-label={placeholder}
-                />
-                <button
-                    type="button"
-                    onClick={handleClear}
-                    className={`absolute right-10 p-2 bg-teal-300 dark:bg-gray-600 text-gray-800 dark:text-gray-200 focus:outline-none transition-all duration-300 ease-in-out ${
-                        query ? 'scale-x-100 opacity-100' : 'scale-x-0 opacity-0'
-                    }`}
-                    style={{ transformOrigin: 'right' }}
-                    aria-label={t('Clear search')}
-                >
-                    <XCircleIcon className="w-5 h-5 text-red-500" />
-                </button>
-                <button
-                    type="submit"
-                    className="p-2 bg-teal-700 dark:bg-gray-800 text-white rounded-r-lg focus:outline-none focus:ring-2 focus:ring-blue-600"
-                    aria-label={t('Submit search')}
-                >
-                    <MagnifyingGlassIcon className="w-5 h-5" />
-                </button>
-            </form>
-        </Box>
+        <form onSubmit={handleSubmit} className="flex items-center p-2 relative" aria-label={t('Search form')}>
+            <SelectBox
+                options={options}
+                selectedOption={options.find(option => option.value === selectedOption.value)!}
+                setSelectedOption={setSelectedOption}
+                buttonClassName={'rounded-l-lg'}
+                aria-label={t('Select search category')}
+            />
+            <input
+                type="text"
+                placeholder={placeholder}
+                value={query}
+                onChange={e => setQuery(e.target.value)}
+                className="flex-grow p-2 bg-teal-200 dark:bg-gray-700 text-gray-800 dark:text-gray-200 focus:outline-none focus:ring-2 focus:ring-blue-600 transition-all duration-300 ease-in-out"
+                aria-label={placeholder}
+            />
+            <button
+                type="button"
+                onClick={handleClear}
+                className={`absolute right-10 p-2 bg-teal-300 dark:bg-gray-600 text-gray-800 dark:text-gray-200 focus:outline-none transition-all duration-300 ease-in-out ${
+                    query ? 'scale-x-100 opacity-100' : 'scale-x-0 opacity-0'
+                }`}
+                style={{transformOrigin: 'right'}}
+                aria-label={t('Clear search')}
+            >
+                <XCircleIcon className="w-5 h-5 text-red-500"/>
+            </button>
+            <button
+                type="submit"
+                className="p-2 bg-teal-700 dark:bg-gray-800 text-white rounded-r-lg focus:outline-none focus:ring-2 focus:ring-blue-600"
+                aria-label={t('Submit search')}
+            >
+                <MagnifyingGlassIcon className="w-5 h-5"/>
+            </button>
+        </form>
     );
 };
 
