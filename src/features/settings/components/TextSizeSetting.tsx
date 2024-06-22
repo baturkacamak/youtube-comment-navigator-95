b92c48fc8405b5c7e4ddb2a923d0f9bf57eb939f
baturkacamak <batisonline@gmail.com>
2024-06-22 16:13:27 +0200

    Refactor: Simplify text size setting management
    - Removed redundant applyTextSize function.
    - Directly dispatched setTextSize action with the new text size value.
    - Updated useSelector hook to fetch text size from the settings object in the store.
    - Improved the initialization of text size from localStorage settings.

--- src/features/settings/components/TextSizeSetting.tsx
+++ src/features/settings/components/TextSizeSetting.tsx
@@ -5,13 +5,13 @@ import { Option } from '../../../types/utilityTypes';
 import { useTranslation } from 'react-i18next';
 import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from '../../../types/rootState';
-import { setTextSize } from '../../../store/store';
-import {getSettings, saveSettings} from "../utils/settingsUtils";
+import { getSettings } from "../utils/settingsUtils";
+import {setTextSize} from "../../../store/store";
 
 const TextSizeSetting: React.FC = () => {
     const { t } = useTranslation();
     const dispatch = useDispatch();
-    const textSize = useSelector((state: RootState) => state.textSize);
+    const textSize = useSelector((state: RootState) => state.settings.textSize);
 
     const textSizeOptions: Option[] = [
         { value: 'text-sm', label: t('Small'), icon: AdjustmentsHorizontalIcon },
@@ -20,20 +20,13 @@ const TextSizeSetting: React.FC = () => {
         { value: 'text-xl', label: t('Extra Large'), icon: ArrowsPointingOutIcon },
     ];
 
-    const applyTextSize = (textSize: string) => {
-        const settings = getSettings();
-        settings.textSize = textSize;
-        saveSettings(settings);
-        dispatch(setTextSize(textSize));
-    };
-
     useEffect(() => {
         const settings = getSettings();
-        applyTextSize(settings.textSize || 'text-base');
+        dispatch(setTextSize(settings.textSize || 'text-base'));
     }, [dispatch]);
 
     const handleTextSizeChange = (option: Option) => {
-        applyTextSize(option.value);
+        dispatch(setTextSize(option.value));
     };
 
     return (
