35ad716597d90f8489e9af5c24739cf2411797f4
baturkacamak <batisonline@gmail.com>
2024-06-30 15:14:49 +0200

    feat: Enhance UX for Advanced Filters
    - Updated the 'SortList' component to change the 'Length' label to 'Word Count'.
    - Improved 'AdvancedFilters' component by adding labels beneath the input fields for Likes, Replies, and Word Count to enhance user experience.
    - Adjusted layout to maintain consistency and clarity across different filters.

--- src/features/sidebar/components/AdvancedFilters.tsx
+++ src/features/sidebar/components/AdvancedFilters.tsx
@@ -57,23 +57,29 @@ const AdvancedFilters: React.FC = () => {
                         <label className="text-gray-800 dark:text-gray-200 ">{t('Likes')}</label>
                     </div>
                     <div className="flex space-x-2">
-                        <input
-                            type="number"
-                            min="0"
-                            max={typeof likesThreshold.max === 'number' ? likesThreshold.max - 1 : undefined}
-                            value={likesThreshold.min}
-                            onChange={(e) => handleLikesThresholdChange('min', parseInt(e.target.value))}
-                            className="bg-neutral-100 dark:bg-gray-700 text-gray-800 dark:text-gray-200 rounded py-1 px-2 w-20"
-                            placeholder={t('Min')}
-                        />
-                        <input
-                            type="number"
-                            min={likesThreshold.min + 1 || 1}
-                            value={likesThreshold.max === Infinity ? '' : likesThreshold.max}
-                            onChange={(e) => handleLikesThresholdChange('max', e.target.value === '' ? Infinity : parseInt(e.target.value))}
-                            className="bg-neutral-100 dark:bg-gray-700 text-gray-800 dark:text-gray-200 rounded py-1 px-2 w-20"
-                            placeholder={t('Max')}
-                        />
+                        <div className="flex flex-col">
+                            <input
+                                type="number"
+                                min="0"
+                                max={typeof likesThreshold.max === 'number' ? likesThreshold.max - 1 : undefined}
+                                value={likesThreshold.min}
+                                onChange={(e) => handleLikesThresholdChange('min', parseInt(e.target.value))}
+                                className="bg-neutral-100 dark:bg-gray-700 text-gray-800 dark:text-gray-200 rounded py-1 px-2 w-20"
+                                placeholder={t('Min')}
+                            />
+                            <label className="text-gray-500 text-xs mt-1">{t('Min Likes')}</label>
+                        </div>
+                        <div className="flex flex-col">
+                            <input
+                                type="number"
+                                min={likesThreshold.min + 1 || 1}
+                                value={likesThreshold.max === Infinity ? '' : likesThreshold.max}
+                                onChange={(e) => handleLikesThresholdChange('max', e.target.value === '' ? Infinity : parseInt(e.target.value))}
+                                className="bg-neutral-100 dark:bg-gray-700 text-gray-800 dark:text-gray-200 rounded py-1 px-2 w-20"
+                                placeholder={t('Max')}
+                            />
+                            <label className="text-gray-500 text-xs mt-1">{t('Max Likes')}</label>
+                        </div>
                     </div>
                 </div>
                 {/* Replies Filter */}
@@ -83,49 +89,61 @@ const AdvancedFilters: React.FC = () => {
                         <label className="text-gray-800 dark:text-gray-200 ">{t('Replies')}</label>
                     </div>
                     <div className="flex space-x-2">
-                        <input
-                            type="number"
-                            min="0"
-                            max={typeof repliesLimit.max === 'number' ? repliesLimit.max - 1 : undefined}
-                            value={repliesLimit.min}
-                            onChange={(e) => handleRepliesLimitChange('min', parseInt(e.target.value))}
-                            className="bg-neutral-100 dark:bg-gray-700 text-gray-800 dark:text-gray-200 rounded py-1 px-2 w-20"
-                            placeholder={t('Min')}
-                        />
-                        <input
-                            type="number"
-                            min={repliesLimit.min + 1 || 1}
-                            value={repliesLimit.max === Infinity ? '' : repliesLimit.max}
-                            onChange={(e) => handleRepliesLimitChange('max', e.target.value === '' ? Infinity : parseInt(e.target.value))}
-                            className="bg-neutral-100 dark:bg-gray-700 text-gray-800 dark:text-gray-200 rounded py-1 px-2 w-20"
-                            placeholder={t('Max')}
-                        />
+                        <div className="flex flex-col">
+                            <input
+                                type="number"
+                                min="0"
+                                max={typeof repliesLimit.max === 'number' ? repliesLimit.max - 1 : undefined}
+                                value={repliesLimit.min}
+                                onChange={(e) => handleRepliesLimitChange('min', parseInt(e.target.value))}
+                                className="bg-neutral-100 dark:bg-gray-700 text-gray-800 dark:text-gray-200 rounded py-1 px-2 w-20"
+                                placeholder={t('Min')}
+                            />
+                            <label className="text-gray-500 text-xs mt-1">{t('Min Replies')}</label>
+                        </div>
+                        <div className="flex flex-col">
+                            <input
+                                type="number"
+                                min={repliesLimit.min + 1 || 1}
+                                value={repliesLimit.max === Infinity ? '' : repliesLimit.max}
+                                onChange={(e) => handleRepliesLimitChange('max', e.target.value === '' ? Infinity : parseInt(e.target.value))}
+                                className="bg-neutral-100 dark:bg-gray-700 text-gray-800 dark:text-gray-200 rounded py-1 px-2 w-20"
+                                placeholder={t('Max')}
+                            />
+                            <label className="text-gray-500 text-xs mt-1">{t('Max Replies')}</label>
+                        </div>
                     </div>
                 </div>
                 {/* Word Count Filter */}
                 <div className="flex flex-col">
                     <div className="flex items-center mb-1">
                         <DocumentTextIcon className="w-5 h-5 text-gray-800 dark:text-gray-200 mr-2" />
-                        <label className="text-gray-800 dark:text-gray-200 ">{t('Words')}</label>
+                        <label className="text-gray-800 dark:text-gray-200 ">{t('Word Count')}</label>
                     </div>
                     <div className="flex space-x-2">
-                        <input
-                            type="number"
-                            min="0"
-                            max={typeof wordCount.max === 'number' ? wordCount.max - 1 : undefined}
-                            value={wordCount.min}
-                            onChange={(e) => handleWordCountChange('min', parseInt(e.target.value))}
-                            className="bg-neutral-100 dark:bg-gray-700 text-gray-800 dark:text-gray-200 rounded py-1 px-2 w-20"
-                            placeholder={t('Min')}
-                        />
-                        <input
-                            type="number"
-                            min={wordCount.min + 1 || 1}
-                            value={wordCount.max === Infinity ? '' : wordCount.max}
-                            onChange={(e) => handleWordCountChange('max', e.target.value === '' ? Infinity : parseInt(e.target.value))}
-                            className="bg-neutral-100 dark:bg-gray-700 text-gray-800 dark:text-gray-200 rounded py-1 px-2 w-20"
-                            placeholder={t('Max')}
-                        />
+                        <div className="flex flex-col">
+                            <input
+                                type="number"
+                                min="0"
+                                max={typeof wordCount.max === 'number' ? wordCount.max - 1 : undefined}
+                                value={wordCount.min}
+                                onChange={(e) => handleWordCountChange('min', parseInt(e.target.value))}
+                                className="bg-neutral-100 dark:bg-gray-700 text-gray-800 dark:text-gray-200 rounded py-1 px-2 w-20"
+                                placeholder={t('Min')}
+                            />
+                            <label className="text-gray-500 text-xs mt-1">{t('Min Words')}</label>
+                        </div>
+                        <div className="flex flex-col">
+                            <input
+                                type="number"
+                                min={wordCount.min + 1 || 1}
+                                value={wordCount.max === Infinity ? '' : wordCount.max}
+                                onChange={(e) => handleWordCountChange('max', e.target.value === '' ? Infinity : parseInt(e.target.value))}
+                                className="bg-neutral-100 dark:bg-gray-700 text-gray-800 dark:text-gray-200 rounded py-1 px-2 w-20"
+                                placeholder={t('Max')}
+                            />
+                            <label className="text-gray-500 text-xs mt-1">{t('Max Words')}</label>
+                        </div>
                     </div>
                 </div>
                 {/* Date & Time Range Filter */}
--- src/features/sidebar/components/SortList.tsx
+++ src/features/sidebar/components/SortList.tsx
@@ -56,7 +56,7 @@ const SortList: React.FC<SortListProps> = ({filters, setFilters}) => {
         },
         {
             name: 'Length',
-            label: t('Length'),
+            label: t('Word Count'),
             icon: <ChartBarIcon className={`w-5 h-5 mr-px ${getAnimationClass('length', 'flash')}`}/>,
             value: 'length'
         },
