b8d6d9a2bd0854fd33c011915184b2ada882415c
baturkacamak <batisonline@gmail.com>
2024-07-21 17:02:05 +0200

    Add utility functions for exporting comments in JSON and CSV formats
    - Implemented formatFileName to generate filenames with the current date and time, and sanitized video titles.
    - Created exportJSON function to handle exporting comments as a JSON file.
    - Developed exportCSV function to handle exporting comments as a CSV file with proper value escaping.

---
+++ src/features/shared/utils/exportUtils.ts
@@ -0,0 +1,43 @@
+import { Comment } from '../../../types/commentTypes';
+
+export const formatFileName = (extension: string, videoTitle: string): string => {
+    const now = new Date();
+    const date = now.toISOString().split('T')[0]; // Get current date in YYYY-MM-DD format
+    const time = now.toTimeString().split(' ')[0].replace(/:/g, '-'); // Get current time in HH-MM-SS format
+    const formattedTitle = videoTitle.replace(/[^a-zA-Z0-9]/g, '_'); // Replace non-alphanumeric characters with underscores
+    return `${formattedTitle}_${date}_${time}.${extension}`;
+};
+
+export const exportJSON = async (comments: Comment[], fileName: string) => {
+    const dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(comments));
+    const downloadAnchorNode = document.createElement('a');
+    downloadAnchorNode.setAttribute("href", dataStr);
+    downloadAnchorNode.setAttribute("download", fileName);
+    document.body.appendChild(downloadAnchorNode);
+    downloadAnchorNode.click();
+    downloadAnchorNode.remove();
+};
+
+export const exportCSV = async (comments: Comment[], fileName: string) => {
+    if (!comments || comments.length === 0) {
+        console.error('No comments to export');
+        return;
+    }
+    const headers = Object.keys(comments[0]).join(',');
+    const csv = [
+        headers,
+        ...comments.map(comment =>
+            Object.values(comment)
+                .map(value => `"${String(value).replace(/"/g, '""')}"`) // Convert value to string and wrap in double quotes, escape existing double quotes
+                .join(',')
+        )
+    ].join('\n');
+
+    const dataStr = "data:text/csv;charset=utf-8," + encodeURIComponent(csv);
+    const downloadAnchorNode = document.createElement('a');
+    downloadAnchorNode.setAttribute("href", dataStr);
+    downloadAnchorNode.setAttribute("download", fileName);
+    document.body.appendChild(downloadAnchorNode);
+    downloadAnchorNode.click();
+    downloadAnchorNode.remove();
+};
