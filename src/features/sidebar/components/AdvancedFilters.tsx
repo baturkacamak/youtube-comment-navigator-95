91705f9433f1e96ddab1154624c534870be1234b
baturkacamak <batisonline@gmail.com>
2024-07-21 17:01:28 +0200

    Integrate ExportButton in AdvancedFilters and Enhance UI
    - Added `comments` prop to AdvancedFilters and ControlPanel components.
    - Integrated ExportButton in AdvancedFilters to allow comment export.
    - Adjusted styles for better UI consistency and responsiveness.
    - Updated import statements for better readability.

--- src/features/sidebar/components/AdvancedFilters.tsx
+++ src/features/sidebar/components/AdvancedFilters.tsx
@@ -1,12 +1,18 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { HandThumbUpIcon, ChatBubbleLeftRightIcon, CalendarDaysIcon, DocumentTextIcon } from "@heroicons/react/24/outline";
 import { useTranslation } from 'react-i18next';
 import { setFilters } from '../../../store/store';
 import { RootState } from '../../../types/rootState';
 import { FilterState } from '../../../types/filterTypes';
+import ExportButton from '../../shared/components/ExportButton'; // Import the ExportButton component
+import { Comment } from '../../../types/commentTypes';
 
-const AdvancedFilters: React.FC = () => {
+interface AdvancedFiltersProps {
+    comments: any[] | Comment[] | undefined; // Define the type of comments
+}
+
+const AdvancedFilters: React.FC<AdvancedFiltersProps> = ({ comments }) => {
     const { t } = useTranslation();
     const dispatch = useDispatch();
     const filters = useSelector((state: RootState) => state.filters);
@@ -47,132 +53,138 @@ const AdvancedFilters: React.FC = () => {
     };
 
     return (
-        <div className="rounded-lg shadow-lg ml-10">
-            <div className="flex gap-6">
-                {/* Likes Filter */}
-                <div className="flex flex-col">
-                    <div className="flex items-center mb-1">
-                        <HandThumbUpIcon className="w-5 h-5 text-gray-800 dark:text-gray-200 mr-2" />
-                        <label className="text-gray-800 dark:text-gray-200 ">{t('Likes')}</label>
-                    </div>
-                    <div className="flex space-x-2">
-                        <div className="flex flex-col">
-                            <input
-                                type="number"
-                                min="0"
-                                max={typeof likesThreshold.max === 'number' ? likesThreshold.max - 1 : undefined}
-                                value={likesThreshold.min}
-                                onChange={(e) => handleLikesThresholdChange('min', parseInt(e.target.value))}
-                                className="bg-neutral-100 dark:bg-gray-700 text-gray-800 dark:text-gray-200 rounded py-1 px-2 w-20 transition-all ease-in-out duration-300"
-                                placeholder={t('Min')}
-                            />
-                            <label className="text-gray-500 text-xs mt-1">{t('Min Likes')}</label>
+        <div className="rounded-lg shadow-lg p-4 bg-white dark:bg-gray-800">
+            <div className="mt-4 space-y-4">
+                <div className="flex gap-6">
+                    {/* Likes Filter */}
+                    <div className="flex flex-col">
+                        <div className="flex items-center mb-1">
+                            <HandThumbUpIcon className="w-5 h-5 text-gray-800 dark:text-gray-200 mr-2" />
+                            <label className="text-gray-800 dark:text-gray-200 ">{t('Likes')}</label>
                         </div>
-                        <div className="flex flex-col">
-                            <input
-                                type="number"
-                                min={likesThreshold.min + 1 || 1}
-                                value={likesThreshold.max === Infinity ? '' : likesThreshold.max}
-                                onChange={(e) => handleLikesThresholdChange('max', e.target.value === '' ? Infinity : parseInt(e.target.value))}
-                                className="bg-neutral-100 dark:bg-gray-700 text-gray-800 dark:text-gray-200 rounded py-1 px-2 w-20 transition-all ease-in-out duration-300"
-                                placeholder={t('Max')}
-                            />
-                            <label className="text-gray-500 text-xs mt-1">{t('Max Likes')}</label>
+                        <div className="flex space-x-2">
+                            <div className="flex flex-col">
+                                <input
+                                    type="number"
+                                    min="0"
+                                    max={typeof likesThreshold.max === 'number' ? likesThreshold.max - 1 : undefined}
+                                    value={likesThreshold.min}
+                                    onChange={(e) => handleLikesThresholdChange('min', parseInt(e.target.value))}
+                                    className="bg-neutral-100 dark:bg-gray-700 text-gray-800 dark:text-gray-200 rounded py-1 px-2 w-20 transition-all ease-in-out duration-300"
+                                    placeholder={t('Min')}
+                                />
+                                <label className="text-gray-500 text-xs mt-1">{t('Min Likes')}</label>
+                            </div>
+                            <div className="flex flex-col">
+                                <input
+                                    type="number"
+                                    min={likesThreshold.min + 1 || 1}
+                                    value={likesThreshold.max === Infinity ? '' : likesThreshold.max}
+                                    onChange={(e) => handleLikesThresholdChange('max', e.target.value === '' ? Infinity : parseInt(e.target.value))}
+                                    className="bg-neutral-100 dark:bg-gray-700 text-gray-800 dark:text-gray-200 rounded py-1 px-2 w-20 transition-all ease-in-out duration-300"
+                                    placeholder={t('Max')}
+                                />
+                                <label className="text-gray-500 text-xs mt-1">{t('Max Likes')}</label>
+                            </div>
+                        </div>
+                    </div>
+                    {/* Replies Filter */}
+                    <div className="flex flex-col">
+                        <div className="flex items-center mb-1">
+                            <ChatBubbleLeftRightIcon className="w-5 h-5 text-gray-800 dark:text-gray-200 mr-2" />
+                            <label className="text-gray-800 dark:text-gray-200 ">{t('Replies')}</label>
+                        </div>
+                        <div className="flex space-x-2">
+                            <div className="flex flex-col">
+                                <input
+                                    type="number"
+                                    min="0"
+                                    max={typeof repliesLimit.max === 'number' ? repliesLimit.max - 1 : undefined}
+                                    value={repliesLimit.min}
+                                    onChange={(e) => handleRepliesLimitChange('min', parseInt(e.target.value))}
+                                    className="bg-neutral-100 dark:bg-gray-700 text-gray-800 dark:text-gray-200 rounded py-1 px-2 w-20 transition-all ease-in-out duration-300"
+                                    placeholder={t('Min')}
+                                />
+                                <label className="text-gray-500 text-xs mt-1">{t('Min Replies')}</label>
+                            </div>
+                            <div className="flex flex-col">
+                                <input
+                                    type="number"
+                                    min={repliesLimit.min + 1 || 1}
+                                    value={repliesLimit.max === Infinity ? '' : repliesLimit.max}
+                                    onChange={(e) => handleRepliesLimitChange('max', e.target.value === '' ? Infinity : parseInt(e.target.value))}
+                                    className="bg-neutral-100 dark:bg-gray-700 text-gray-800 dark:text-gray-200 rounded py-1 px-2 w-20 transition-all ease-in-out duration-300"
+                                    placeholder={t('Max')}
+                                />
+                                <label className="text-gray-500 text-xs mt-1">{t('Max Replies')}</label>
+                            </div>
+                        </div>
+                    </div>
+                    {/* Word Count Filter */}
+                    <div className="flex flex-col">
+                        <div className="flex items-center mb-1">
+                            <DocumentTextIcon className="w-5 h-5 text-gray-800 dark:text-gray-200 mr-2" />
+                            <label className="text-gray-800 dark:text-gray-200 ">{t('Word Count')}</label>
+                        </div>
+                        <div className="flex space-x-2">
+                            <div className="flex flex-col">
+                                <input
+                                    type="number"
+                                    min="0"
+                                    max={typeof wordCount.max === 'number' ? wordCount.max - 1 : undefined}
+                                    value={wordCount.min}
+                                    onChange={(e) => handleWordCountChange('min', parseInt(e.target.value))}
+                                    className="bg-neutral-100 dark:bg-gray-700 text-gray-800 dark:text-gray-200 rounded py-1 px-2 w-20 transition-all ease-in-out duration-300"
+                                    placeholder={t('Min')}
+                                />
+                                <label className="text-gray-500 text-xs mt-1">{t('Min Words')}</label>
+                            </div>
+                            <div className="flex flex-col">
+                                <input
+                                    type="number"
+                                    min={wordCount.min + 1 || 1}
+                                    value={wordCount.max === Infinity ? '' : wordCount.max}
+                                    onChange={(e) => handleWordCountChange('max', e.target.value === '' ? Infinity : parseInt(e.target.value))}
+                                    className="bg-neutral-100 dark:bg-gray-700 text-gray-800 dark:text-gray-200 rounded py-1 px-2 w-20 transition-all ease-in-out duration-300"
+                                    placeholder={t('Max')}
+                                />
+                                <label className="text-gray-500 text-xs mt-1">{t('Max Words')}</label>
+                            </div>
+                        </div>
+                    </div>
+                    {/* Date & Time Range Filter */}
+                    <div className="flex flex-col">
+                        <div className="flex items-center mb-1">
+                            <CalendarDaysIcon className="w-5 h-5 text-gray-800 dark:text-gray-200 mr-2" />
+                            <label className="text-gray-800 dark:text-gray-200 ">{t('Date & Time Range')}</label>
+                        </div>
+                        <div className="flex space-x-2">
+                            <div className="flex flex-col">
+                                <input
+                                    type="datetime-local"
+                                    value={dateTimeRange.start}
+                                    onChange={(e) => handleDateTimeRangeChange('start', e.target.value)}
+                                    className="bg-neutral-100 dark:bg-gray-700 text-gray-800 dark:text-gray-200 rounded p-1 w-full transition-all ease-in-out duration-300"
+                                />
+                                <label className="text-gray-500 text-xs mt-1">{t('Start')}</label>
+                            </div>
+                            <div className="flex flex-col">
+                                <input
+                                    type="datetime-local"
+                                    value={dateTimeRange.end}
+                                    min={dateTimeRange.start} // Ensure end cannot be before start
+                                    max={new Date().toISOString().slice(0, 16)} // Current date and time
+                                    onChange={(e) => handleDateTimeRangeChange('end', e.target.value)}
+                                    className="bg-neutral-100 dark:bg-gray-700 text-gray-800 dark:text-gray-200 rounded p-1 w-full transition-all ease-in-out duration-300"
+                                />
+                                <label className="text-gray-500 text-xs mt-1">{t('End')}</label>
+                            </div>
                         </div>
                     </div>
                 </div>
-                {/* Replies Filter */}
-                <div className="flex flex-col">
-                    <div className="flex items-center mb-1">
-                        <ChatBubbleLeftRightIcon className="w-5 h-5 text-gray-800 dark:text-gray-200 mr-2" />
-                        <label className="text-gray-800 dark:text-gray-200 ">{t('Replies')}</label>
-                    </div>
-                    <div className="flex space-x-2">
-                        <div className="flex flex-col">
-                            <input
-                                type="number"
-                                min="0"
-                                max={typeof repliesLimit.max === 'number' ? repliesLimit.max - 1 : undefined}
-                                value={repliesLimit.min}
-                                onChange={(e) => handleRepliesLimitChange('min', parseInt(e.target.value))}
-                                className="bg-neutral-100 dark:bg-gray-700 text-gray-800 dark:text-gray-200 rounded py-1 px-2 w-20 transition-all ease-in-out duration-300"
-                                placeholder={t('Min')}
-                            />
-                            <label className="text-gray-500 text-xs mt-1">{t('Min Replies')}</label>
-                        </div>
-                        <div className="flex flex-col">
-                            <input
-                                type="number"
-                                min={repliesLimit.min + 1 || 1}
-                                value={repliesLimit.max === Infinity ? '' : repliesLimit.max}
-                                onChange={(e) => handleRepliesLimitChange('max', e.target.value === '' ? Infinity : parseInt(e.target.value))}
-                                className="bg-neutral-100 dark:bg-gray-700 text-gray-800 dark:text-gray-200 rounded py-1 px-2 w-20 transition-all ease-in-out duration-300"
-                                placeholder={t('Max')}
-                            />
-                            <label className="text-gray-500 text-xs mt-1">{t('Max Replies')}</label>
-                        </div>
-                    </div>
-                </div>
-                {/* Word Count Filter */}
-                <div className="flex flex-col">
-                    <div className="flex items-center mb-1">
-                        <DocumentTextIcon className="w-5 h-5 text-gray-800 dark:text-gray-200 mr-2" />
-                        <label className="text-gray-800 dark:text-gray-200 ">{t('Word Count')}</label>
-                    </div>
-                    <div className="flex space-x-2">
-                        <div className="flex flex-col">
-                            <input
-                                type="number"
-                                min="0"
-                                max={typeof wordCount.max === 'number' ? wordCount.max - 1 : undefined}
-                                value={wordCount.min}
-                                onChange={(e) => handleWordCountChange('min', parseInt(e.target.value))}
-                                className="bg-neutral-100 dark:bg-gray-700 text-gray-800 dark:text-gray-200 rounded py-1 px-2 w-20 transition-all ease-in-out duration-300"
-                                placeholder={t('Min')}
-                            />
-                            <label className="text-gray-500 text-xs mt-1">{t('Min Words')}</label>
-                        </div>
-                        <div className="flex flex-col">
-                            <input
-                                type="number"
-                                min={wordCount.min + 1 || 1}
-                                value={wordCount.max === Infinity ? '' : wordCount.max}
-                                onChange={(e) => handleWordCountChange('max', e.target.value === '' ? Infinity : parseInt(e.target.value))}
-                                className="bg-neutral-100 dark:bg-gray-700 text-gray-800 dark:text-gray-200 rounded py-1 px-2 w-20 transition-all ease-in-out duration-300"
-                                placeholder={t('Max')}
-                            />
-                            <label className="text-gray-500 text-xs mt-1">{t('Max Words')}</label>
-                        </div>
-                    </div>
-                </div>
-                {/* Date & Time Range Filter */}
-                <div className="flex flex-col">
-                    <div className="flex items-center mb-1">
-                        <CalendarDaysIcon className="w-5 h-5 text-gray-800 dark:text-gray-200 mr-2" />
-                        <label className="text-gray-800 dark:text-gray-200 ">{t('Date & Time Range')}</label>
-                    </div>
-                    <div className="flex space-x-2">
-                        <div className="flex flex-col">
-                            <input
-                                type="datetime-local"
-                                value={dateTimeRange.start}
-                                onChange={(e) => handleDateTimeRangeChange('start', e.target.value)}
-                                className="bg-neutral-100 dark:bg-gray-700 text-gray-800 dark:text-gray-200 rounded p-1 w-full transition-all ease-in-out duration-300"
-                            />
-                            <label className="text-gray-500 text-xs mt-1">{t('Start')}</label>
-                        </div>
-                        <div className="flex flex-col">
-                            <input
-                                type="datetime-local"
-                                value={dateTimeRange.end}
-                                min={dateTimeRange.start} // Ensure end cannot be before start
-                                max={new Date().toISOString().slice(0, 16)} // Current date and time
-                                onChange={(e) => handleDateTimeRangeChange('end', e.target.value)}
-                                className="bg-neutral-100 dark:bg-gray-700 text-gray-800 dark:text-gray-200 rounded p-1 w-full transition-all ease-in-out duration-300"
-                            />
-                            <label className="text-gray-500 text-xs mt-1">{t('End')}</label>
-                        </div>
-                    </div>
+                <hr className="border-solid border-t" />
+                <div className="flex justify-start">
+                    <ExportButton comments={comments} />
                 </div>
             </div>
         </div>
