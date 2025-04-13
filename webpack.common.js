aea10b66a4463bfca85e62d28eb0c09a7f867b7e
baturkacamak <batisonline@gmail.com>
2025-04-13 17:16:23 +0200

    feat: add SCSS support and optimize comment appending in store
    - Updated webpack config to support SCSS by modifying the CSS rule to include `.scss` files and adding `sass-loader`.
    - Improved performance of `addProcessedReplies` reducer by appending comments using `push` instead of array spread, reducing memory usage and improving efficiency.

--- webpack.common.js
+++ webpack.common.js
@@ -20,8 +20,8 @@ module.exports = {
                 },
             },
             {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader', 'postcss-loader'],
+                test: /\.(css|scss)$/,
+                use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader'],
             },
         ],
     },
